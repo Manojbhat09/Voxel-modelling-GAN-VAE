dfacc4883d6147852e9ad5d80fa6c4d0eead6535
Mateusz Podzorski <podzorskimateusz@gmail.com>
2020-05-02 20:33:12 -0400

    Add files via upload

--- SegFault_DataSet.py
+++ SegFault_DataSet.py
@@ -4,63 +4,61 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
-class VoxelDataSet(Dataset):
+class trainData(Dataset):
+    def __init__(self):
+        super(trainData, self).__init__()
+        # ModelNet10 provides a dataset of models in the form of .OFF files. A voxelized form of the dataset was provided by http://aguo.us/writings/classify-modelnet.html.
 
-	def __init__(self, path='./voxel_data'):
-		super(VoxelDataSet, self).__init__()
-        self.voxels = np.array() # Storage for voxel objects
-		voxels_fn = [afn for afn in os.listdir(path) if afn.endswith('.dat')]
+        voxData = np.load('modelnet10.npz')
 
-        # Get Voxel Arrays
-        initY = False
-        initX = False
-        initV = False
-        with open(os.path.join(path, voxels_fn[0]), 'r', encoding="utf8", errors='ignore') as f:
-			raw_data = f.readlines()
+        xTest = voxData['X_test']  # ndarray of size (908, 30, 30, 30)
+        xTrain = voxData['X_train']  # ndarray of size (3991, 30, 30, 30)
+        yTest = voxData['y_test']  # ndarray of size (908, )
+        yTrain = voxData['y_train']  # ndarray of size (3991, )
 
-            voxel_ph = 
-			for obj in range(len(raw_data)):
-                current_voxel = raw_data[obj].split(' ')
+        bcelabels = np.zeros((3991, 10))
 
-				while "" in current_voxel:
-					current_voxel.remove("")
-                #This schema assumes the data lists, in order, each z value for a given y, 
-                # for each y at a given x, for each x
-                for x in range(32):
-                    for y in range (32):
-                        temp_1d = []
-                        for z in range(32):
-                            temp_1d.append(current_voxel.pop(0)) #List of z values for a given y and x
-                        
-                        np_1d = np.array(temp_1d) #Z values for a given y, 1-D
+        for i in range(len(yTrain)):
+            idx = yTrain[i]
+            bcelabels[i][idx] = 1
 
-                        if initY == False:
-                            np_2d = np.array(temp_1d)
-                            initY = True
-                        else:
-                            np.concatenate(np_2d, np_1d) #Assembles z values for each y at a given x, 2-D
-                    
-                    initY = False #Resets z row storage on next run
+        self.xData = xTrain
+        self.yData = yTrain
+        self.BCELabels = bcelabels #Each label consists of a 10-length vector where the corresponding index of the desired object class is 1 and everything else is zero.
 
-                    if initX == False:
-                        np_3d = np_2d
-                        initX = True
-                    else:
-                        np.concatenate(np_3d, np_2d) #Assembles zy planes for each x, 3-D
-                
-                initX = False #Resets zy plane storage on next run
+    def __getitem__(self, idx):
+        return self.xData[idx], self.yData[idx], self.BCELabels[idx]
 
-                if initV == False:
-                    voxel_ph = np_3d
-                    initV = True
-                else:
-                    np.concatenate(voxel_ph, np_3d)
+    def __len__(self):
+        return len(self.yData)
 
-        self.voxels = voxel_ph #4 dimensional np array
 
-	def __getitem__(self, idx):
-		return self.voxels[idx]
-		
-	def __len__(self):
-		return len(self.voxels)
+if __name__ == "__main__":
+    dataset = trainData()
 
+
+class testData(Dataset):
+    def __init__(self):
+        super(testData, self).__init__()
+        # ModelNet10 provides a dataset of models in the form of .OFF files. A voxelized form of the dataset was provided by http://aguo.us/writings/classify-modelnet.html.
+
+        voxData = np.load('modelnet10.npz')
+        # print(dataset.files)
+
+        xTest = voxData['X_test']  # ndarray of size (908, 30, 30, 30)
+        xTrain = voxData['X_train']  # ndarray of size (3991, 30, 30, 30)
+        yTest = voxData['y_test']  # ndarray of size (908, )
+        yTrain = voxData['y_train']  # ndarray of size (3991, )
+
+        self.xData = xTest
+        self.yData = yTest
+
+    def __getitem__(self, idx):
+        return self.xData[idx], self.yData[idx], self.testLabels[idx]
+
+    def __len__(self):
+        return len(self.yData)
+
+
+if __name__ == "__main__":
+    dataset = trainData()
--- SegFault_train_VAE.py
+++ SegFault_train_VAE.py
@@ -3,68 +3,115 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 import numpy as np
-
-from SegFault_DataSet import VoxelDataSet
+import matplotlib.pyplot as plt
 from SegFault_VAE import VAE
-from utils import *
 
 from torch.nn import functional as F
+
+from SegFault_DataSet import trainData
+from SegFault_DataSet import testData
 
 
 def main():
     device = 'cpu'
 
-    # define dataset and dataloader
-    dataset = VoxelDataSet()
-    voxel_dataloader = DataLoader(dataset, batch_size=20, shuffle=True)
-
     # Definer Hyperparameters
-    latent_dim = 100
+    latent_dim = 1000
     lr = 0.005         # learning rate
-    num_epochs = 100
+    num_epochs = 0
+    batch_dim = 307 #13 or 307
+    gamma = 0.97
+
+    #Load Data
+    #dataset = VoxelDataSet()
+    #voxel_dataloader = DataLoader(dataset, batch_size=20, shuffle=True)
+
+    trainDataSet = trainData()
+    trainLoader = DataLoader(dataset=trainDataSet, batch_size=batch_dim, shuffle=True, num_workers=0)
+    testDataSet = testData()
+    testLoader = DataLoader(dataset=testDataSet, batch_size=batch_dim, shuffle=True, num_workers=0)
 
     # Build the model
     vae = VAE(object_dim=32, latent_dim=latent_dim).to(device)
-    print("VAE model:\n", vae)
 
     #Loss Function
     def getLoss(reconstructedData, data, mu, std):
 
+
         MSELoss = F.mse_loss(reconstructedData, data)
 
-        KLDivergence = -0.5 * torch.sum(1 + std - mu.pow(2) - std.exp())
 
-        a = 3 #alpha, weight of KL Divergence
+        #print("  MSE: ", MSELoss.item())
 
-        loss = MSELoss + a * KLDivergence
+        #print("         std: ", std)
+        #print("         mu: ", mu)
+
+        #KLDivergence = -0.5 * torch.sum(1 + std - mu.pow(2) - std.exp())
+
+        #print("  KLD: ", KLDivergence.item())
+
+        #a = 1 #alpha, weight of KL Divergence
+
+        #loss = MSELoss + a * KLDivergence
+        loss = MSELoss
+
+        #print("  Loss: ", loss.item())
 
         return loss
 
+    def newLoss(generatedData, targetData):
+
+        loss = 0
+
+        return loss
+
+
     # define optimizer for discriminator and generator separately
-    optim = Adam(vae.parameters(), lr=lr)
+    optim = Adam(vae.parameters(), lr=lr, weight_decay=1e-4)
 
     epochVals = []
     lossVals = []
-    
+
+    print("BEGIN TRAINING")
+
     #Training
     for epoch in range(num_epochs):
-        for n_batch, (local_batch, __) in enumerate(voxel_dataloader):
-            voxel_labels = local_batch.to(device)
+        for n_batch, (x_batch, labels, BCELabels) in enumerate(trainLoader): #x_batch are the actual voxel objects, the labels are the modelnet object classes, ints from 0-9. The labels aren't terribly important.
+                                                                    
+            #print("    Batch ", n_batch)
 
-            output, mu, std = vae(voxel_labels) 
+            voxel_labels = labels.to(device)
+            x_batch = x_batch.to(device)
 
-            loss = getLoss(output, voxel_labels, mu, std)
+            #x_batch is of dimensions (batch_size, 30, 30, 30). Need to add a dimension for channels.
+
+            x_batch = x_batch.unsqueeze(1) 
+            x_batch = x_batch.float()
+
+            output, mu, std, l_z = vae(x_batch) 
+
+            print(output.shape)
+
+            #Get Labels
+            BCELabels = BCELabels.to(device)
+
+            loss = getLoss(output, x_batch, mu, std)
 
             optim.zero_grad()
             loss.backward()
             optim.step()
 
-            if (n_batch + 1) % 30 == 0:
+            #print("     Loss, ", loss )
+            
+            if (n_batch + 1) % 4 == 0:
                 print("Epoch: [{}/{}], Batch: {}, loss: {}".format(
                     epoch, num_epochs, n_batch, loss.item()))
 
         epochVals = epochVals + [epoch]
         lossVals = lossVals + [loss]
+        print("Epoch ", epoch)
+
+    print("TRAINING FINISHED")
 
     torch.save(vae.state_dict(), 'vae_model.ckpt')
 
@@ -75,7 +122,46 @@ def main():
     plt.ylabel('Losses')
     plt.show()
 
-    #Reconstruct Objects, Output Text File
+    #Reconstruct Objects
+
+    reconstructedVoxels = torch.empty(0, 1, 30, 30, 30)
+
+    print("BEGIN RECONSTRUCTION")
+
+    for n_batch, (x_batch, labels, BCELabels) in enumerate(trainLoader): 
+        x_batch = x_batch.to(device)
+        x_batch = x_batch.unsqueeze(1) 
+        x_batch = x_batch.float()
+
+        output, mu, std, l_z = vae(x_batch) 
+
+        x_batch = x_batch.squeeze()
+
+        reconstructedVoxels = torch.cat((reconstructedVoxels, output), 0)
+        #print("Batch ", n_batch)
+        #print(type(reconstructedVoxels[0,0,0,0,0].item()))
+
+    print(reconstructedVoxels.shape)
+
+    #Write to File
+
+    outF = open("Generated_Voxels.txt", "w")
+    outF.write("Generated Voxel Data \n")
+    outF.write("Data formatted as: (0,0,0), (0,0,1), (0,0,2), ... (0,0,30), (0,1,0), (0,1,1), ... \n")
+    for idx in range(3991):
+        outF.write("[")
+        for x in range(30):
+            for y in range(30):
+                for z in range(30):
+                    currentElem = reconstructedVoxels[idx,0,x,y,z].item()
+                    if currentElem > 0.5
+                        outF.write(str(1))
+                    if currentElem <= 0.5
+                        outF.write(str(0))
+                    outF.write(",")
+        outF.write("] \n")
+
+    outF.close()
 
 
 if __name__ == "__main__":

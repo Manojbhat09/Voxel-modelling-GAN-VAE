a1c3e12a7ebc1c268e5777230a83e25e555e32f5
Byungju Lee <qnach10@gmail.com>
2020-05-04 10:27:36 -0400

    update gui

--- gui/main.cpp
+++ gui/main.cpp
@@ -55,6 +55,8 @@ public:
 	void SetUpCameraTransformation(void);
 
 	void GetForwardVector(double& vx, double& vy, double& vz);
+	void GetUpVector(double& vx, double& vy, double& vz);
+	void GetRightVector(double& vx, double& vy, double& vz);
 };
 
 CameraObject::CameraObject()
@@ -71,9 +73,9 @@ void CameraObject::Initialize(void)
 	p = 0;
 	b = 0;
 
-	fov = YsPi / 6.0;  // 30 degree
+	fov = YsPi / 3.0;  // 30 degree
 	nearZ = 0.1;
-	farZ = 200.0;
+	farZ = 20000.0;
 }
 
 void CameraObject::SetUpCameraProjection(void)
@@ -106,45 +108,42 @@ void CameraObject::GetForwardVector(double& vx, double& vy, double& vz)
 	vz = -cos(p) * cos(h);
 }
 
-
-class OrbitingViewer
+void CameraObject::GetUpVector(double& vx, double& vy, double& vz)
 {
-public:
-	double h, p;
-	double dist;
-	double focusX, focusY, focusZ;
+	const double ux0 = 0.0;
+	const double uy0 = 1.0;
+	const double uz0 = 0.0;
 
-	OrbitingViewer();
-	void Initialize(void);
-	void SetUpCamera(CameraObject& camera);
-};
+	const double ux1 = cos(b) * ux0 - sin(b) * uy0;
+	const double uy1 = sin(b) * ux0 + cos(b) * uy0;
+	const double uz1 = uz0;
 
-OrbitingViewer::OrbitingViewer()
-{
-	Initialize();
+	const double ux2 = ux1;
+	const double uy2 = cos(p) * uy1 - sin(p) * uz1;
+	const double uz2 = sin(p) * uy1 + cos(p) * uz1;
+
+	vx = cos(-h) * ux2 - sin(-h) * uz2;
+	vy = uy2;
+	vz = sin(-h) * ux2 + cos(-h) * uz2;
 }
 
-void OrbitingViewer::Initialize(void)
+void CameraObject::GetRightVector(double& vx, double& vy, double& vz)
 {
-	h = 0;
-	p = 0;
-	dist = 100.0;
-	focusX = 0.0;
-	focusY = 0.0;
-	focusZ = 0.0;
-}
+	const double ux0 = 1.0;
+	const double uy0 = 0.0;
+	const double uz0 = 0.0;
 
-void OrbitingViewer::SetUpCamera(CameraObject& camera)
-{
-	camera.h = h;
-	camera.p = p;
-	camera.b = 0.0;
+	const double ux1 = cos(b) * ux0 - sin(b) * uy0;
+	const double uy1 = sin(b) * ux0 + cos(b) * uy0;
+	const double uz1 = uz0;
 
-	double vx, vy, vz;
-	camera.GetForwardVector(vx, vy, vz);
-	camera.x = focusX - vx * dist;
-	camera.y = focusY - vy * dist;
-	camera.z = focusZ - vz * dist;
+	const double ux2 = ux1;
+	const double uy2 = cos(p) * uy1 - sin(p) * uz1;
+	const double uz2 = sin(p) * uy1 + cos(p) * uz1;
+
+	vx = cos(-h) * ux2 - sin(-h) * uz2;
+	vy = uy2;
+	vz = sin(-h) * ux2 + cos(-h) * uz2;
 }
 
 void DrawCube(double x1, double y1, double z1, double x2, double y2, double z2, int color)
@@ -312,22 +311,17 @@ vector<vector<vector<int>>> generateTestVector(vector<vector<vector<int>>> vec)
 {
 	
 
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < 30; i++)
 	{
 		vec.push_back(vector<vector<int>>());
-		for (int j = 0; j < 32; j++)
+		for (int j = 0; j < 30; j++)
 		{
 			vec[i].push_back(vector<int>());
-			for (int k = 0; k < 32; k++)
+			for (int k = 0; k < 30; k++)
 			{
-				if (i == j == k)
-				{
-					vec[i][j].push_back(1);
-				}
-				else
-				{
-					vec[i][j].push_back(0);
-				}
+				
+				vec[i][j].push_back(0);
+				
 			}
 		}
 	}
@@ -368,14 +362,29 @@ class FsLazyWindowApplication : public FsLazyWindowApplicationBase
 protected:
 	bool needRedraw;
 	CameraObject camera;
-	OrbitingViewer orbit;
-	int wid = 800;
-	int hei = 600;
-
+	int wid = 1280;
+	int hei = 760;
+	float rotation = 0;
+	int currentNum = 0;
 	/* test part*/
 	vector<vector<vector<int>>> testVec;
-	//vector<vector<int>> voxDat(35, vector<int>(27000));
+	vector<vector<vector<int>>> testVec1;
+	vector<vector<vector<int>>> testVec2;
+	vector<vector<vector<int>>> testVec3;
+	vector<vector<vector<int>>> testVec4;
+	vector<vector<vector<int>>> testVec5;
 
+	vector<vector<vector<int>>> resultVec;
+	vector<vector<vector<int>>> resultVec1;
+	vector<vector<vector<int>>> resultVec2;
+	vector<vector<vector<int>>> resultVec3;
+	vector<vector<vector<int>>> resultVec4;
+	vector<vector<vector<int>>> resultVec5;
+	vector<int> linedata;
+
+	vector<vector<int>> voxelData;
+	//vector<vector<int>> voxel_data;
+	//vector<vector<int>> voxelData(35, vector<int>(27000));
 	vector<vector<vector<vector<int>>>> testVoxelData; //4d vector of dimensions (voxel index, x, y, z)
 
 public:
@@ -404,15 +413,21 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 {
 	opt.x0 = 0;
 	opt.y0 = 0;
-	opt.wid = wid;
-	opt.hei = hei;
+	opt.wid = 1280;
+	opt.hei = 760;
 }
 /* virtual */ void FsLazyWindowApplication::Initialize(int argc, char* argv[])
 {
-	camera.z = 10.0;
+	camera.z = 50;
 
 	ifstream fin;
 	vector<vector<int>> voxelData(35, vector<int>(27000));
+	testVec = generateTestVector(testVec);
+	testVec1 = generateTestVector(testVec1);
+	testVec2 = generateTestVector(testVec2);
+	testVec3 = generateTestVector(testVec3);
+	testVec4 = generateTestVector(testVec4);
+	testVec5 = generateTestVector(testVec5);
 
 	fin.open("Target_Voxels.txt");
 	for (int i = 0; i < 35; i++)
@@ -424,11 +439,76 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 		}
 	}
 
-	//cout << voxelData[5].size() << endl;
+	cout << voxelData[3].size() << endl;
 	//cout << voxelData[6].size() << endl;
 	//cout << voxelData[7].size() << endl;
+	/**/
+	int totalIdx = 0;
+	for (int i = 0; i < 30; i++)
+	{
+		for (int j = 0; j < 30; j++)
+		{
+			for (int k = 0; k < 30; k++)
+			{
+				//cout << totalIdx << endl;
+				testVec[i][j][k] = voxelData[0][totalIdx];
+				testVec1[i][j][k] = voxelData[1][totalIdx];
+				testVec2[i][j][k] = voxelData[2][totalIdx];
+				testVec3[i][j][k] = voxelData[3][totalIdx];
+				testVec4[i][j][k] = voxelData[4][totalIdx];
+				testVec5[i][j][k] = voxelData[5][totalIdx];
+				totalIdx++;
+			}
+		}
+	}
 
-	vector<vector<vector<vector<int>>>> voxContainer(35, vector<vector<vector<int>>>(30,vector<vector<int>>(30,vector<int>(30))));
+
+	ifstream fin1;
+	vector<vector<int>> voxelData1(35, vector<int>(27000));
+	resultVec = generateTestVector(resultVec);
+	resultVec1 = generateTestVector(resultVec2);
+	resultVec2 = generateTestVector(resultVec2);
+	resultVec3 = generateTestVector(resultVec3);
+	resultVec4 = generateTestVector(resultVec4);
+	resultVec5 = generateTestVector(resultVec5);
+
+	fin1.open("Generated_Voxels.txt");
+	for (int i = 0; i < 35; i++)
+	{
+		for (int j = 0; j < 27000; j++)
+		{
+			fin1 >> voxelData1[i][j];
+			//cout << voxelData[i][j];
+		}
+	}
+
+	cout << voxelData1[3].size() << endl;
+	//cout << voxelData[6].size() << endl;
+	//cout << voxelData[7].size() << endl;
+	/**/
+	int totalIdx1 = 0;
+	for (int i = 0; i < 30; i++)
+	{
+		for (int j = 0; j < 30; j++)
+		{
+			for (int k = 0; k < 30; k++)
+			{
+				//cout << totalIdx << endl;
+				resultVec[i][j][k] = voxelData1[0][totalIdx1];
+				resultVec1[i][j][k] = voxelData1[1][totalIdx1];
+				resultVec2[i][j][k] = voxelData1[2][totalIdx1];
+				resultVec3[i][j][k] = voxelData1[3][totalIdx1];
+				resultVec4[i][j][k] = voxelData1[4][totalIdx1];
+				resultVec5[i][j][k] = voxelData1[5][totalIdx1];
+				totalIdx1++;
+			}
+		}
+	}
+
+
+
+	/*
+	vector<vector<vector<vector<int>>>> voxContainer(35, vector<vector<vector<int>>>(30, vector<vector<int>>(30, vector<int>(30))));
 
 	for (int v = 0; v < 30; v++)
 	{
@@ -450,13 +530,22 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 	cout << voxContainer[5][5].size() << endl;
 	cout << voxContainer[5][5][5].size() << endl;
 
-	/*std::ifstream file("Target_Voxels.txt");
+	*/
+
+	/*
+	std::ifstream file("Target_Voxels.txt");
 	std::string str;
 	char delimiter =' ';
 	int lines = 0;
 	int skiplines = 0;
-	vector<vector<int>> voxel_data;
 	int numLines = 0;
+	//if (file.is_open()) {
+	//	while (std::getline(file, str)) {
+	//		numLines++;
+	//	}
+	//}
+	//std::cout << "Lines " << numLines << "\n";
+	//voxel_data.resize(numLines);
 	if (file.is_open()) {
 		while (std::getline(file, str)) {
 			skiplines++;
@@ -469,10 +558,10 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 
 			std::istringstream tokenStream(str);
 			std::string token;
-			std::vector<int> linedata;
+			
 			while (std::getline(tokenStream, token, delimiter)) {
 				//std::cout << "word num " << words << "\n";
-				//std::cout << token<<"\n";
+				std::cout << token<<"\n";
 				int dat = std::stoi(token);
 				//std::cout << dat<<"\n";
 				linedata.push_back(dat);
@@ -480,7 +569,7 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 				words++;
 			}
 			//voxel_data[lines] = linedata;
-			voxel_data.push_back(linedata);
+			voxel_data[1].push_back(linedata);
 			lines++;
 		}
 		std::cout << "line done \n";
@@ -490,9 +579,43 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 		std::cout << "file is not open\n";
 	}
 	std::cout << "closing done \n";
-	file.close();*/
+	file.close();
 
 
+	cout << voxel_data[1].size() << endl;
+	cout << voxel_data[2].size() << endl;
+	cout << voxel_data[3].size() << endl;
+	cout << voxel_data[4].size() << endl;
+	cout << voxel_data[5].size() << endl;
+	cout << voxel_data[6].size() << endl;
+	*/
+	/*
+	int totalIdx = 0;
+	for (int i = 0; i < 30; i++)
+	{
+		for (int j = 0; j < 30; j++)
+		{
+			for (int k = 0; k < 30; k++)
+			{
+				testVec[i][j][k] = voxel_data[5][totalIdx];
+				totalIdx++;
+			}
+		}
+	}
+	totalIdx = 0;
+
+	for (int i = 0;  i < 30; i++)
+	{
+		for (int j = 0; j < 30; j++)
+		{
+			for (int k = 0; k < 30;  k++)
+			{
+				cout<<"hey: %d\n" <<voxel_data[5][totalIdx] << endl;
+				totalIdx++;
+			}
+		}
+	}
+	*/
 	/*
 	ifstream fin("Target_Voxels.txt");
 	int v = 0;
@@ -562,34 +685,71 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 	}
 	if (0 != FsGetKeyState(FSKEY_LEFT))
 	{
-		orbit.h += YsPi / 180.0;
+		camera.h += YsPi / 180.0;
+		cout << "cam.h: " << camera.h;
 	}
 	if (0 != FsGetKeyState(FSKEY_RIGHT))
 	{
-		orbit.h -= YsPi / 180.0;
+		camera.h -= YsPi / 180.0;
+		cout << "cam.h: " << camera.h;
 	}
 	if (0 != FsGetKeyState(FSKEY_UP))
 	{
-		orbit.p += YsPi / 180.0;
+		camera.p += YsPi / 180.0;
+		cout << "cam.p: " << camera.p;
 	}
 	if (0 != FsGetKeyState(FSKEY_DOWN))
 	{
-		orbit.p -= YsPi / 180.0;
+		camera.p -= YsPi / 180.0;
+		cout << "cam.p: " << camera.p;
 	}
-	if (0 != FsGetKeyState(FSKEY_F) && orbit.dist > 1.0)
+	if (0 != FsGetKeyState(FSKEY_F))
 	{
-		orbit.dist /= 1.02;
+		double vx, vy, vz;
+		camera.GetForwardVector(vx, vy, vz);
+		camera.x += vx * 0.5;
+		camera.y += vy * 0.5;
+		camera.z += vz * 0.5;
+		cout << "cam.x: " << camera.x << " cam.y: " << camera.y << " cam.z: " << camera.z;
 	}
-	if (0 != FsGetKeyState(FSKEY_B) && orbit.dist < 200.0)
+	if (0 != FsGetKeyState(FSKEY_B))
 	{
-		orbit.dist *= 1.02;
+		double vx, vy, vz;
+		camera.GetForwardVector(vx, vy, vz);
+		camera.x -= vx * 0.5;
+		camera.y -= vy * 0.5;
+		camera.z -= vz * 0.5;
+
+	}
+	if (0 != FsGetKeyState(FSKEY_Q))
+	{
+		if (currentNum < 5)
+		{
+			currentNum += 1;
+		}
+		else if (currentNum == 5)
+		{
+			currentNum = 0;
+		}
+	}
+	if (0 != FsGetKeyState(FSKEY_W))
+	{
+		if (currentNum > 0)
+		{
+			currentNum -= 1;
+		}
+		else if (currentNum == 0)
+		{
+			currentNum = 5;
+		}
 	}
 	else
 	{
-		orbit.h -= (YsPi / 180.0)*0.1f;
+		rotation += 0.5;
+		glRotatef(rotation, 0.0f, 0.0f, 1.0f);
+		//glRotatef(rotation, 0.0f, 1.0f, 0.0f);
+		glRotatef(rotation, 1.0f, 0.0f, 0.0f);
 	}
-	orbit.SetUpCamera(camera);
-
 
 
 	needRedraw = true;
@@ -600,7 +760,7 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-	glViewport(0, 0, wid, hei);
+	glViewport(0, 0, 1280, 760);
 
 	// Set up 3D drawing
 	camera.SetUpCameraProjection();
@@ -613,24 +773,81 @@ FsLazyWindowApplication::FsLazyWindowApplication()
 	// 3D drawing from here
 	//DrawCube(-1.0, -1.0, -1.0, 1.0, 1.0, 1.0);
 
-	/*test part*/
-	//LoopingInput(generateTestVector(testVec));
 
-	//LoopingInputWithXYZ(generateTestVector(testVec), -20, 0, 0, 1, 0);
-	//LoopingInputWithXYZ(generateTestVector(testVec), 20, 0, 0, 2, 1);
+
+
+
+	rotation += 0.;
+	glRotatef(rotation, 0.0f, 0.0f, 1.0f);
+	//glRotatef(rotation, 0.0f, 1.0f, 0.0f);
+	glRotatef(rotation, 1.0f, 0.0f, 0.0f);
+
+	/*test part*/
+	//LoopingInput(testVec);
+
+	//glTranslatef(1280 / 2, 760 / 2, 0); // M1 - 2nd translation
+	//glRotatef(30, 0.0f, 0.0f, 1.0f);                  // M2
+	//glTranslatef(1280 / 2, 760 / 2, 0);  // M3 - 1st translation
+	if (currentNum == 0)
+	{
+		LoopingInputWithXYZ(generateTestVector(testVec), -15, 0, 0, 1, 0);
+		LoopingInputWithXYZ(generateTestVector(resultVec), 15, 0, 0, 1, 1);
+	} 
+	else if(currentNum == 1)
+	{
+		LoopingInputWithXYZ(generateTestVector(testVec1), -15, 0, 0, 1, 0);
+		LoopingInputWithXYZ(generateTestVector(resultVec1), 15, 0, 0, 1, 1);
+	}
+	else if (currentNum == 2)
+	{
+		LoopingInputWithXYZ(generateTestVector(testVec2), -15, 0, 0, 1, 0);
+		LoopingInputWithXYZ(generateTestVector(resultVec2), 15, 0, 0, 1, 1);
+	}
+	else if (currentNum == 3)
+	{
+		LoopingInputWithXYZ(generateTestVector(testVec3), -15, 0, 0, 1, 0);
+		LoopingInputWithXYZ(generateTestVector(resultVec3), 15, 0, 0, 1, 1);
+	}
+	else if (currentNum == 4)
+	{
+		LoopingInputWithXYZ(generateTestVector(testVec4), -15, 0, 0, 1, 0);
+		LoopingInputWithXYZ(generateTestVector(resultVec4), 15, 0, 0, 1, 1);
+	}
+	else if (currentNum == 5)
+	{
+		LoopingInputWithXYZ(generateTestVector(testVec5), -15, 0, 0, 1, 0);
+		LoopingInputWithXYZ(generateTestVector(resultVec5), 15, 0, 0, 1, 1);
+	}
+	//LoopingInputWithXYZ(generateTestVector(testVec2), 30, 0, -100, 1, 2);
+	//LoopingInputWithXYZ(generateTestVector(testVec3),  60, 0, -100, 1, 3);
+	//LoopingInputWithXYZ(generateTestVector(testVec4), 90, 0, -100, 1, 4);
+	//LoopingInputWithXYZ(generateTestVector(testVec5), 120, 0, -100, 1, 5);
 
 	/*voxel test part*/
 	//HandlingVoxelData(testVoxelData);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 	arrangeText();
 
 	// Set up 2D drawing
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, (float)wid - 1, (float)hei - 1, 0, -1, 1);
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	//glOrtho(0, (float)wid - 1, (float)hei - 1, 0, -1, 1);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	//glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
 
 	glDisable(GL_DEPTH_TEST);
 
